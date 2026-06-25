12f62faa3fbd15c8a6f5b6deceb6f206dbcebee9
jack <jackmj@proton.me>
2026-06-25 18:45:31 -0500

    Added option to disable nftables for Tailscale.

--- modules/nixos/tailscale/default.nix
+++ modules/nixos/tailscale/default.nix
@@ -1,5 +1,4 @@
 {
-    pkgs,
     config,
     lib,
     ...
@@ -9,27 +8,37 @@ with lib; {
         enable = mkOption {
             type = types.bool;
             default = false;
-            description = "Enable zero confiuration VPN using modern nftables.";
+            description = "Enable zero configuration VPN using modern nftables.";
+        };
+        enableLegacyTables = mkOption {
+            type = types.bool;
+            default = false;
+            description = "Use legacy iptables instead of modern nftables.";
         };
     };
+    config = mkIf (config.pos.tailscale.enable && config.pos.enable) (mkMerge [
+        # Enable the Tailscale service.
+        {
+            services.tailscale.enable = true;
+        }
 
-    config = mkIf (config.pos.tailscale.enable && config.pos.enable) {
-        # Enable the service and the firewall.
-        services.tailscale.enable = true;
-        networking.nftables.enable = true;
-        networking.firewall = {
-            enable = true;
-            trustedInterfaces = [config.services.tailscale.interfaceName];
-            allowedUDPPorts = [config.services.tailscale.port];
-        };
+        # Enable the firewall and force nftables when not using legacy tables.
+        (mkIf (!config.pos.tailscale.enableLegacyTables) {
+            networking.nftables.enable = true;
+            networking.firewall = {
+                enable = true;
+                trustedInterfaces = [config.services.tailscale.interfaceName];
+                allowedUDPPorts = [config.services.tailscale.port];
+            };
 
-        # Force tailscaled to use nftables.
-        systemd.services.tailscaled.serviceConfig.Environment = [
-            "TS_DEBUG_FIREWALL_MODE=nftables"
-        ];
+            # Force tailscaled to use nftables.
+            systemd.services.tailscaled.serviceConfig.Environment = [
+                "TS_DEBUG_FIREWALL_MODE=nftables"
+            ];
 
-        # Prevent systemd from waiting for network online.
-        systemd.network.wait-online.enable = false;
-        boot.initrd.systemd.network.wait-online.enable = false;
-    };
+            # Prevent systemd from waiting for network online.
+            systemd.network.wait-online.enable = false;
+            boot.initrd.systemd.network.wait-online.enable = false;
+        })
+    ]);
 }
